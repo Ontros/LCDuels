c1d6afa0b7ce56431ab91082635c9d72aec22b1a
Ontros <ontro512@gmail.com>
2024-06-11 23:30:57 +0200

    Initial commit

---
+++ LCDuels/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCDuels.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        //[HarmonyPatch(nameof(PlayerControllerB.Update))] if public member
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patchUpate(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ LCDuels/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCDuels.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCDuels
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LCDuelsModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "onty.duels";
+        private const string modName = "LCDuels";
+        private const string modVersion = "0.1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LCDuelsModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("LC Duels has been awaken");
+
+            harmony.PatchAll(typeof(LCDuelsModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
